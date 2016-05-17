63a186826394c29ebb0e60df28e85179ce063b63
Jones Sean <t4001987@MACLA208088.local>
2016-05-17 08:16:56 +0100

    jade template added

--- app.js
+++ app.js
@@ -3,10 +3,11 @@ var app = express();
 var port = process.env.PORT || 5000;
 
 app.use(express.static('public'));
-app.use(express.static('src/views'));
+app.set('views', './src/views');
+app.set('view engine', 'jade');
 
 app.get('/', function(req, res) {
-    res.send('Hello world');
+    res.render('index');
 });
 
 app.get('/test', function(req, res) {

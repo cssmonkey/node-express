a685f02ddf92a0aceb2d95f84d26cec4ff223d7f
cssmonkey <web@sean-jones.co.uk>
2016-05-25 08:34:01 +0100

    started using routes

--- app.js
+++ app.js
@@ -1,16 +1,30 @@
 var express = require('express');
 var app = express();
 var port = process.env.PORT || 5000;
+var bookRouter = express.Router();
 
 app.use(express.static('public'));
 app.set('views', './src/views');
 app.set('view engine', 'ejs');
 
+bookRouter.route('/')
+  .get(function(req, res){
+    res.send('Hello books');
+  });
+
+bookRouter.route('/single')
+  .get(function(req, res){
+    res.send('Hello single book');
+  });
+
+app.use('/Books', bookRouter);
+
 app.get('/', function(req, res) {
     res.render('index', {
       title: 'App title goes here',
       nav: [{
-        Link: '/Books', Text: 'Books'
+        Link: '/Books',
+        Text: 'Books'
       }, {
         Link: '/Authors',
         Text: 'Authors'
@@ -18,9 +32,6 @@ app.get('/', function(req, res) {
     });
 });
 
-app.get('/books', function(req, res) {
-    res.send('Hello Books'); 
-});
 
 app.listen(port, function(err){
     console.log('running server on port' + port);
